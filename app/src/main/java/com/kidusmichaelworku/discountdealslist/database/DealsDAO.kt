f3bafdcd4ab0dc00837a3a5d4e6c714611863df9
kidusmik <kidusmik@gmail.com>
2023-06-24 23:18:30 +0300

    Add the suspend keyword to support couroutines

--- app/src/main/java/com/kidusmichaelworku/discountdealslist/database/DealsDAO.kt
+++ app/src/main/java/com/kidusmichaelworku/discountdealslist/database/DealsDAO.kt
@@ -12,17 +12,17 @@ import androidx.room.Update
 @Dao
 interface DealsDAO {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insertDeal(dealsModel: DealsModel)
+    suspend fun insertDeal(dealsModel: DealsModel)
 
     @Query("SELECT * FROM favorites_table")
-    fun getAllDeals(): LiveData<List<DealsModel>>
+    suspend fun getAllDeals(): LiveData<List<DealsModel>>
 
     @Query("SELECT * FROM favorites_table WHERE lmd_id == :id")
-    fun getSelectedDeal(id: Int): DealsModel
+    suspend fun getSelectedDeal(id: Int): DealsModel
 
     @Delete
-    fun deleteDeal(dealsModel: DealsModel)
+    suspend fun deleteDeal(dealsModel: DealsModel)
 
     @Update
-    fun updateDeal(dealsModel: DealsModel)
+    suspend fun updateDeal(dealsModel: DealsModel)
 }
