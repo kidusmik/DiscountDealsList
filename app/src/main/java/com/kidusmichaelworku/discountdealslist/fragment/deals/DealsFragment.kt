7f2c320f6816940972d1f65e61112d7920f705c6
kidusmik <kidusmik@gmail.com>
2023-06-25 23:05:32 +0300

    Update deals fragment

--- app/src/main/java/com/kidusmichaelworku/discountdealslist/fragment/deals/DealsFragment.kt
+++ app/src/main/java/com/kidusmichaelworku/discountdealslist/fragment/deals/DealsFragment.kt
@@ -1,18 +1,15 @@
 package com.kidusmichaelworku.discountdealslist.fragment.deals
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.kidusmichaelworku.discountdealslist.R
 import com.kidusmichaelworku.discountdealslist.databinding.FragmentDealsBinding
-import com.kidusmichaelworku.discountdealslist.services.Deals
 import com.kidusmichaelworku.discountdealslist.services.DealsNetwork
 import com.kidusmichaelworku.discountdealslist.services.DealsService
 import kotlinx.coroutines.CoroutineScope
@@ -61,11 +58,13 @@ class DealsFragment : Fragment() {
          //Use coroutines to make the API calls in a separate thread
         CoroutineScope (Dispatchers.IO).launch {
             // Get list of deals
-            val deals = dealsService.getDeals ()
+            val deals = dealsService.getOffers()
+            Log.d("Result is", deals.toString())
+
 
             // Do something with the results on the main thread
             withContext (Dispatchers.Main) {
-                val rvDealsAdapter = DealsListRecyclerAdapter(deals, dealsViewModel)
+                val rvDealsAdapter = DealsListRecyclerAdapter(deals.offers, dealsViewModel)
 
                 binding.rvDealsList.layoutManager = layoutManager
                 binding.rvDealsList.adapter = rvDealsAdapter
@@ -73,10 +72,10 @@ class DealsFragment : Fragment() {
         }
         //val rvDeals: RecyclerView = binding.rvDealsList
 
-//        val deal1 = Deals(1275984, "timesprime.com","https://www.timesprime.com/", "Times Prime Membership @ just &#8377;1199", "Times Prime Membership", "Get it at just &#8377;1199","","", "Entertainment,Entertainment Subscription","No","N", "https://www.timesprime.com/", "https://smartlink.linkmydeals.com/?account_id=18950&network=&url=https%3A%2F%2Fwww.timesprime.com%2F", "https://static.timesprime.com/2x/youtube-logo-box.png","Deal","Offer","Hot Offer","new", "2023-06-19","2023-06-27")
-//        val deal2 = Deals(12345678, "toliday.in","https://www.toliday.in/","Get Flat &#8377;300 discount on Flight and Hotel Booking","Get &#8377;300 OFF","This voucher is valid on Flight and Hotel Booking","FLAT300","","Travel,Domestic Flights,International Flights,Hotels","No","N","https://toliday.in/","https://smartlink.linkmydeals.com/?account_id=18950&network=&url=https%3A%2F%2Ftoliday.in%2F","https://toliday.in/coupon-code/10.png","Code","Price-Off","&#8377;300","new","2023-02-01","2023-06-25")
+//        val deal1 = Offers(1275984, "timesprime.com","https://www.timesprime.com/", "Times Prime Membership @ just &#8377;1199", "Times Prime Membership", "Get it at just &#8377;1199","","", "Entertainment,Entertainment Subscription","No","N", "https://www.timesprime.com/", "https://smartlink.linkmydeals.com/?account_id=18950&network=&url=https%3A%2F%2Fwww.timesprime.com%2F", "https://static.timesprime.com/2x/youtube-logo-box.png","Deal","Offer","Hot Offer","new", "2023-06-19","2023-06-27")
+//        val deal2 = Offers(12345678, "toliday.in","https://www.toliday.in/","Get Flat &#8377;300 discount on Flight and Hotel Booking","Get &#8377;300 OFF","This voucher is valid on Flight and Hotel Booking","FLAT300","","Travel,Domestic Flights,International Flights,Hotels","No","N","https://toliday.in/","https://smartlink.linkmydeals.com/?account_id=18950&network=&url=https%3A%2F%2Ftoliday.in%2F","https://toliday.in/coupon-code/10.png","Code","Price-Off","&#8377;300","new","2023-02-01","2023-06-25")
 //
-//        val dealList: List<Deals> = listOf(deal1, deal2)
+//        val dealList: List<Offers> = listOf(deal1, deal2)
 
 //        var dealsService: DealsService =
 
--- app/src/main/java/com/kidusmichaelworku/discountdealslist/fragment/deals/DealsListRecyclerAdapter.kt
+++ app/src/main/java/com/kidusmichaelworku/discountdealslist/fragment/deals/DealsListRecyclerAdapter.kt
@@ -3,15 +3,13 @@ package com.kidusmichaelworku.discountdealslist.fragment.deals
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.kidusmichaelworku.discountdealslist.database.DealsModel
 import com.kidusmichaelworku.discountdealslist.databinding.LiDealsListBinding
-import com.kidusmichaelworku.discountdealslist.services.Deals
+import com.kidusmichaelworku.discountdealslist.services.Offers
 
-class DealsListRecyclerAdapter(private val dealsList: List<Deals>, private val viewModel: DealsViewModel)
+class DealsListRecyclerAdapter(private val offersList: List<Offers>, private val viewModel: DealsViewModel)
     : RecyclerView.Adapter<DealsListRecyclerAdapter.DealsListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DealsListViewHolder {
@@ -20,29 +18,29 @@ class DealsListRecyclerAdapter(private val dealsList: List<Deals>, private val v
         return DealsListViewHolder(binding)
     }
 
-    override fun getItemCount() = dealsList.size
+    override fun getItemCount() = offersList.size
 
     override fun onBindViewHolder(holder: DealsListViewHolder, position: Int) {
         with(holder){
-            with(dealsList[position]) {
-                binding.tvDescriptionDeals.text = dealsList[position].description
-                binding.tvTitleDeals.text = dealsList[position].title
-                binding.tvCouponCodeDeals.text = dealsList[position].code
-                binding.tvDiscountedPriceDeals.text = dealsList[position].offer_value
+            with(offersList[position]) {
+                binding.tvDescriptionDeals.text = offersList[position].description
+                binding.tvTitleDeals.text = offersList[position].title
+                binding.tvCouponCodeDeals.text = offersList[position].code
+                binding.tvDiscountedPriceDeals.text = offersList[position].offer_value
 
                 //val hours = "$hours learning hours, $country"
                 //binding.topLearnerTime.text = hours
                 Glide.with(holder.itemView.context)
-                    .load(dealsList[position].image_url)
+                    .load(offersList[position].image_url)
                     .into(binding.ivDeals)
 
                 binding.imageButton.setOnClickListener {
                     val addedDeal = DealsModel(
-                        lmd_id = dealsList[position].lmd_id,
-                        description = dealsList[position].description,
-                        title = dealsList[position].title,
-                        code = dealsList[position].code,
-                        offer_value = dealsList[position].offer_value
+                        lmd_id = offersList[position].lmd_id,
+                        description = offersList[position].description,
+                        title = offersList[position].title,
+                        code = offersList[position].code,
+                        offer_value = offersList[position].offer_value
                     )
 
                     viewModel.addDeal(addedDeal)
@@ -51,6 +49,7 @@ class DealsListRecyclerAdapter(private val dealsList: List<Deals>, private val v
                 }
 
                 holder.itemView.setOnClickListener {
+                    //it.findNavController().navigate()
                     Toast.makeText(holder.itemView.context, "Clicked an Item",
                         Toast.LENGTH_SHORT).show()
                 }
