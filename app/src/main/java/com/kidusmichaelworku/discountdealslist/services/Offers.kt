6f393158b9d340c943002d9c7424d0332638ba31
kidusmik <kidusmik@gmail.com>
2023-06-26 14:11:39 +0300

    Make Offers class parcelable

--- app/src/main/java/com/kidusmichaelworku/discountdealslist/services/Offers.kt
+++ app/src/main/java/com/kidusmichaelworku/discountdealslist/services/Offers.kt
@@ -1,5 +1,8 @@
 package com.kidusmichaelworku.discountdealslist.services
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Offers (
     val lmd_id: Int,
     val store: String?,
@@ -21,4 +24,65 @@ data class Offers (
     val status: String?,
     val start_date: String?,
     val end_date: String?
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(lmd_id)
+        parcel.writeString(store)
+        parcel.writeString(merchant_homepage)
+        parcel.writeString(long_offer)
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(code)
+        parcel.writeString(terms_and_conditions)
+        parcel.writeString(categories)
+        parcel.writeString(featured)
+        parcel.writeString(publisher_exclusive)
+        parcel.writeString(url)
+        parcel.writeString(smartlink)
+        parcel.writeString(image_url)
+        parcel.writeString(type)
+        parcel.writeString(offer)
+        parcel.writeString(offer_value)
+        parcel.writeString(status)
+        parcel.writeString(start_date)
+        parcel.writeString(end_date)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Offers> {
+        override fun createFromParcel(parcel: Parcel): Offers {
+            return Offers(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Offers?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
